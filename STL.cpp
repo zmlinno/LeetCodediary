88c1d67b9c64e5773e57c6835076d47aa101130b
zmlinno <131747526+zmlinno@users.noreply.github.com>
2024-12-28 00:00:26 +0900

    Add files via upload

---
+++ STL.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<cmath>
+using namespace std;
+
+
+//关于STL的to_string函数
+//将整数，浮点数等数字类型转换为std::string类型
+
+// int main()
+// {
+//     int a = 10,b = 20;
+//     string num = to_string(a);
+//     string num1 = to_string(b);
+    
+//     double p = 3.14159;
+//     string num2 = to_string(p);
+
+//     cout<<num<<num1<<num2<<endl;
+//     cout<<endl;
+//     return 0;
+// }
+
+
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int sum = 0;
+//     int num = 0;
+//     if(n&2 != 0)
+//     {
+        
+//         while(n>0)
+//         {
+//             sum += n;
+//             n = n-2;
+//         }
+//         cout<<sum<<endl;
+//     }
+//     else
+//     {
+//         while(n>0)
+//         {
+//             num += pow(n,2);
+//             n = n-2;
+//         }
+//         cout<<num<<endl;
+//     }
+//     cout<<endl;
+//     return 0;
+
+// }
+
+
+
+
